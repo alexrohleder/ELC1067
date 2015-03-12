078c413ab1b06a5295855ae4678e895f145cbfdb
alexrohleder96 <alexrohleder96@outlook.com>
2015-03-12 11:44:39 -0300

    Primeiro Trabalho

--- T1/t1.c
+++ T1/t1.c
@@ -4,25 +4,84 @@
 // if (c == "\n")
 // feof(file)
 
+void ler_alunos(int * matriculas, char ** nome, int * n) {
+	int matricula, i = 0, j = 0;
+	char caracter, nome[50];
+
+	FILE * resource = fopen("alunos.txt", "r");
+
+	if (resource == NULL) {
+		printf("Erro ao abrir um dos arquivos."); return;
+	}
+
+	while (feof(resource) != 0) {
+		if (fscanf(resource, "%d", &matricula)) {
+			break;
+		}
+
+		caracter = fgetc(resource);
+
+		while (caracter != "\n") {
+			nome[i] = caracter;
+			caracter = fgetc(resource);
+			/**/ i++;
+		}
+
+		nome[i] = "\n";
+		matriculas[j] = matricula;
+		strcpy(nomes[j], nome);
+		/**/ j++;
+	}
+
+	fclose(resource);
+	n = linha;
+}
+
+float media(int * matricula) {
+	int m;
+	float n1, n2;
+
+	FILE * resource = fopen("notas.txt", "r");
+
+	if (resource == NULL) {
+		printf("Erro ao abrir um dos arquivos."); return;
+	}
+
+	while (feof(resource) != 0) {
+		if (fscanf(resource, "%d", &m)) {
+			break;
+		}
+
+		if (m == matricula) {
+			if (fscanf(resource, "%f", &n1) && fcanf(resource, "%f", &n2)) {
+				return (n1 + n2) / 2;
+			}
+		}
+	}
+
+	return 0.0;
+}
+
 void main (int argc, char ** argv) {
 
-	int matriculas[50], i;
-	char nomes[50][64], caracter;
-	float n1, n2, media;
-
-	FILE * alunos, notas;
+	int matriculas[50], matricula, linhas, linha, i;
+	char nomes[50][64], nome[64], caracter;
 
 	if (argc > 1) {
 		printf("Você deve definir um nome de aluno como parâmetro."); return;
 	}
 
-	alunos = fopen("alunos.txt", "r");
-	notas = fopen("notas.txt", "r");
+	ler_alunos(&matriculas, &nomes, &linhas);
 
-	if (alunos == NULL || notas == NULL) {
-		printf("Erro ao abrir um dos arquivos."); return;
+	for (i = 0; i <= linhas; i++) {
+		if (strcmp(nomes[i], nome) >= 0) {
+			linha = i; break;
+		}
 	}
 
-	fclose(alunos);
-	fclose(notas);
+	matricula = matriculas[linha];
+	nome = nomes[linha];
+
+	printf("Aluno %s(%d) tem média: %f", nome, matricula, media(matricula));
+
 }
