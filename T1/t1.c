0fe63d18c610f611bf115d371f569ac77902ffe6
alexrohleder96 <alexrohleder96@outlook.com>
2015-03-16 11:54:40 -0300

    Primeiro Trabalho

--- T1/t1.c
+++ T1/t1.c
@@ -2,37 +2,54 @@
 #include <string.h>
 
 void main(int argc, char ** argv) {
-	int i, j, matriculas[50];
-	char c, nome[50][255];
-	float notas[2], media;
+	int i, j, k, matriculas[50];
+	char c, nomes[50][255];
+	float notas[50][2], media;
 
 	if (argc < 2) {
 		printf("Você deve definir qual nome será buscado."); return;
 	}
 
-	FILE * alunos = fopen("alunos.txt", "r");
-	FILE * notas  = fopen("notas.txt", "r");
+	FILE * falunos = fopen("alunos.txt", "r");
+	FILE * fnotas  = fopen("notas.txt", "r");
 
-	if (alunos == NULL || notas == NULL) {
+	if (falunos == NULL || fnotas == NULL) {
 		printf("Erro ao abrir os arquivos necessários."); return;
 	}
 
 	i = j = 0;
 
-	while (!feof(alunos) != 0) {
-		if (fscanf(alunos, "%d", &matriculas[i])) {
+	while (!feof(falunos) != 0) {
+		if (fscanf(falunos, "%d", &matriculas[i]) <= 0) {
 			break;
 		}
 
-		c = fgetc(alunos);
+		c = fgetc(falunos);
 
-		while (c != "\n") {
-			nome[i][j] = c;
-			c = fgetc(alunos);
+		while (c != '\n') {
+			nomes[i][j] = c;
+			c = fgetc(falunos);
 			j++;
 		}
 
-		
+		nomes[i][j] = '\0';
+		i++;
+	}
+
+	i = j = 0;
+
+	while (!feof(fnotas) != 0) {
+		if (fscanf(fnotas, "%d %f %f", &matriculas[i], &notas[i][0], &notas[i][1]) <= 0) {
+			break;
+		}
+
+		i++;
+	}
+
+	for (j = 0; j < i; j++) {
+		if (strstr(argv[1], nomes[j]) == NULL) {
+			printf("%.2f %s\n", (notas[j][0] + notas[j][1]) / 2, nomes[j]);
+		}
 	}
 }
 
